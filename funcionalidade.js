2ef48f797b1e7a7b28b722df6f644699eff50866
Thayna-Andrade <thayyyandrade@gmail.com>
2025-04-24 17:40:47 +0000

    att

--- funcionalidade.js
+++ funcionalidade.js
@@ -1,14 +1,14 @@
-function lerIdade(){
+function lerIdade() {
     var idade = parseInt(document.getElementById("entrada").value);
+
+    votacao(idade);
 }
 
-votacao(idade);
-
-function votacao(idade){
-    if(idade < 18){
-        document.getElementById("resposta").innerHTML = `<div class="tile">Idade informada: ${idade}</div>`
+function votacao(idade) {
+    document.getElementById("resposta").innerHTML = `<div class="tile">Idade informada: ${idade}</div>`
+    if (idade < 18) {
         document.getElementById("resposta").innerHTML += `<div class="tile">A pessoa não pode votar</div>`
     } else {
-         document.getElementById("resposta").innerHTML = `<div class="tile">Idade informada: ${idade}</div>`
+        document.getElementById("resposta").innerHTML += `<div class="tile">A pessoa pode votar</div>`
     }
-    }
+}
